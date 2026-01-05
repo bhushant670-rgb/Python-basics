8fb4e9e17ce897b93082bcfca44819ce3373917f
bhushant670-rgb <bhushant670@gmail.com>
2026-01-05 12:35:49 +0530

    Add first python program

---
+++ hello.py
@@ -0,0 +1,15 @@
+# First python program
+#Author : Bhushan
+print("Hello Github")
+print("This is my first python file")
+
+Name="Bhushan"
+print(f"My name is {Name}")
+print(f"Hello {Name}")
+
+age=21
+if age >= 18:
+  print("You are eligible for voting")
+else:
+  print("You are not eligible for voting")
+
